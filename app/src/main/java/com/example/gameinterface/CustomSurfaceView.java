4b7a726ed38fc43020806eb0ea1c5ba69ecd666c
dewey <a874589@gmail.com>
2021-02-20 20:59:12 -0800

    Made more progress on the drawCard functions and have partially completed a textLineWrap function for later use

--- app/src/main/java/com/example/gameinterface/CustomSurfaceView.java
+++ app/src/main/java/com/example/gameinterface/CustomSurfaceView.java
@@ -1,12 +1,15 @@
 package com.example.gameinterface;
 
 import android.graphics.PixelFormat;
+import android.graphics.Rect;
 import android.view.SurfaceView;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Paint;
+
+import java.util.ArrayList;
 
 public class CustomSurfaceView extends SurfaceView {
 
@@ -21,7 +24,9 @@ public class CustomSurfaceView extends SurfaceView {
     }
 
     protected void onDraw(Canvas canvas){
-        drawFighterCard(canvas, 250, 1000, "Skeleton", 7, 3);
+        drawFighterCard(canvas, 100, 200, "Skeleton", 7, 3);
+        drawSpellCard(canvas, 400, 200, "Might", 2, 1,+3,
+                false, "", false);
     }
 
     //draws a fighter card on the screen
@@ -52,24 +57,96 @@ public class CustomSurfaceView extends SurfaceView {
     }
 
     //draws a spell card on the screen
+    //if hasCardText is true the card is printed with textEffect at the bottom
+    //else if hasCardText is false the card only has a power modifier
+    //spell types: 1 = direct, 2 = enchant, 3 = support
+    protected void drawSpellCard(Canvas canvas, float x, float y, String spellName, int mana,
+                                 int spellType, int powerMod, boolean hasCardText,
+                                 String textEffect, boolean isForbidden){
+        drawCardOutline(canvas, x, y);
+        drawCardTitle(canvas, x, y, spellName);
+
+        //draws a symbol according to the spell's type
+        //colored circles are used as stand ins for now
+        Paint symbolPaint = new Paint();
+        switch(spellType){
+            case 1:
+                symbolPaint.setColor(Color.RED);
+                break;
+            case 2:
+                symbolPaint.setColor(Color.BLUE);
+                break;
+            case 3:
+                symbolPaint.setColor(Color.YELLOW);
+                break;
+        }
+        canvas.drawCircle(x + 30.0f, y + 90.0f, 15.0f, symbolPaint);
+
+        //draws mana cost on the card
+        Paint manaTextPaint = new Paint();
+        manaTextPaint.setColor(Color.BLACK);
+        manaTextPaint.setTextAlign(Paint.Align.CENTER);
+        manaTextPaint.setTextSize(40);
+        manaTextPaint.setAntiAlias(true);
+        canvas.drawText(Integer.toString(mana), x + 195.0f, y + 105f, manaTextPaint);
+
+        //draws a symbol (green circle place holder is fine for now) under the spell type symbol
+        //if the isForbidden parameter is true
+
+        //draws the effect text at the bottom of the card if hasCardText is true
+
+        //draws the power modifier on the bottom of the card if hasCardText is false (no cards
+        //with unique card text modify power as far as I have seen)
+
+    }
 
     //draws a face down card on the screen
+    protected void drawFaceDownCard(Canvas canvas, float x, float y, Color cardBack){
+        //draw a filled square with the color cardBack at the x y coordinate
+        //reminder the origin refers to the top left of the card
+
+        drawCardOutline(canvas, x, y);
+
+        //prints the string cheaty mages across the card (diagonally bottom left to top right)
+        //horizontally in two lines would also work but diagonally should be able to be done with
+        //drawTextOnPath
+    }
 
     //draws the black border for the card outline
     protected void drawCardOutline(Canvas canvas, float x, float y){
-        Paint cardOutline = new Paint();
-        cardOutline.setColor(Color.BLACK);
-        cardOutline.setStyle(Paint.Style.STROKE);
-        cardOutline.setStrokeWidth(10.0f);
-        canvas.drawRect(x, y, x + 225.0f, y + 300.0f, cardOutline);
+        Paint outlinePaint = new Paint();
+        outlinePaint.setColor(Color.BLACK);
+        outlinePaint.setStyle(Paint.Style.STROKE);
+        outlinePaint.setStrokeWidth(10.0f);
+        canvas.drawRect(x, y, x + 225.0f, y + 300.0f, outlinePaint);
     }
 
     //draws the card name at the top of the card
     protected void drawCardTitle(Canvas canvas, float x, float y, String text){
-        Paint titleText = new Paint();
-        titleText.setColor(Color.BLACK);
-        titleText.setTextSize(50);
-        titleText.setTextAlign(Paint.Align.CENTER);
-        canvas.drawText(text, x + 112.5f, y + 60.0f, titleText);
+        Paint titlePaint = new Paint();
+        titlePaint.setColor(Color.BLACK);
+        titlePaint.setTextSize(40);
+        titlePaint.setTextAlign(Paint.Align.CENTER);
+        titlePaint.setAntiAlias(true);
+        canvas.drawText(text, x + 112.5f, y + 60.0f, titlePaint);
+    }
+
+    //converts a string into an array of smaller strings using a max pixel width
+    //this is intended be used to wrap text within a certain width
+    protected ArrayList<String> textLineWrap(String text, float width, Paint textPaint){
+        Rect bounds = new Rect();
+        ArrayList<String> splitText = new ArrayList<String>();
+        int textStart = 0;
+        while(textStart < text.length()) {
+            for (int i = 1; i <= text.length(); i++) {
+                textPaint.getTextBounds(text, textStart, i, bounds);
+                if(bounds.width() > width) {
+                    splitText.add(text.substring(textStart, i));
+                    textStart = i;
+                    break;
+                }
+            }
+        }
+        return splitText;
     }
 }
