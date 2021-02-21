eba8e240b2a96d373d9776eb82da187176ce63e4
dewey <a874589@gmail.com>
2021-02-20 16:29:00 -0800

    Added a bunch of drawCard functions to the custom surface view

--- app/src/main/java/com/example/gameinterface/CustomSurfaceView.java
+++ app/src/main/java/com/example/gameinterface/CustomSurfaceView.java
@@ -1,5 +1,6 @@
 package com.example.gameinterface;
 
+import android.graphics.PixelFormat;
 import android.view.SurfaceView;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
@@ -12,12 +13,39 @@ public class CustomSurfaceView extends SurfaceView {
     public CustomSurfaceView(Context context, AttributeSet attrs){
         super(context, attrs);
         setWillNotDraw(false);
+
+        //makes the surface view background to transparent
+        //code is adapted from https://stackoverflow.com/a/27959612
+        this.setZOrderOnTop(true);
+        this.getHolder().setFormat(PixelFormat.TRANSLUCENT);
     }
 
     protected void onDraw(Canvas canvas){
-        Paint red = new Paint();
-        red.setColor(Color.RED);
-        canvas.drawRect(0, 0, 100.0f, 100.0f, red);
+        drawFighterCard(canvas, 25, 25, "Skeleton");
+    }
+
+    protected void drawFighterCard(Canvas canvas, float x, float y, String fighterName){
+        drawCardOutline(canvas, x, y);
+        drawCardTitle(canvas, x, y, fighterName);
+    }
+
+    //draws the black border for the card outline
+    protected void drawCardOutline(Canvas canvas, float x, float y){
+        Paint cardOutline = new Paint();
+        cardOutline.setColor(Color.BLACK);
+        cardOutline.setStyle(Paint.Style.STROKE);
+        cardOutline.setStrokeWidth(10.0f);
+        canvas.drawRect(x, y, x + 225.0f, y + 300.0f, cardOutline);
+    }
+
+    //draws the card name at the top of the card
+    //x and y are the top left corner of the card
+    protected void drawCardTitle(Canvas canvas, float x, float y, String text){
+        Paint titleText = new Paint();
+        titleText.setColor(Color.BLACK);
+        titleText.setTextSize(40);
+        titleText.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText(text, x + 112.5f, y + 50.0f, titleText);
     }
 
 }
